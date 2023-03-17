d6829ab917cd26dfaa63224697c4f552ebd53fde
tzquocuong918 <72614483+tzquocuong918@users.noreply.github.com>
2023-03-17 15:14:12 +0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,14 +11,31 @@ provider "azurerm" {
   use_msi = true
 }
 
+# provider "azurerm" {
+#   alias           = "management"
+#   subscription_id = local.subscription_id_management
+#   features {}
+# }
+
+# provider "azurerm" {
+#   alias           = "connectivity"
+#   subscription_id = local.subscription_id_connectivity
+#   features {}
+# }
 provider "azurerm" {
   alias           = "management"
-  subscription_id = local.subscription_id_management
+  subscription_id   = "3f87c349-6fa2-455b-bd5b-1222f6d15367"
+  tenant_id         = "037f8e6b-2776-49f6-bd0f-234af6a8030b"
+  client_id         = "17d948e8-5a27-499f-a64c-ca6481ec5439"
+  client_secret     = "kSe8Q~aH.~E4En0gmX2NxQ13_7oFJ7SX-w-kGbUk"
   features {}
 }
 
 provider "azurerm" {
   alias           = "connectivity"
-  subscription_id = local.subscription_id_connectivity
+  subscription_id   = "3f87c349-6fa2-455b-bd5b-1222f6d15367"
+  tenant_id         = "037f8e6b-2776-49f6-bd0f-234af6a8030b"
+  client_id         = "17d948e8-5a27-499f-a64c-ca6481ec5439"
+  client_secret     = "kSe8Q~aH.~E4En0gmX2NxQ13_7oFJ7SX-w-kGbUk"
   features {}
 }
